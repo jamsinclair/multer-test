bead8af0d3441f4453f8c47afe331a97a62f5027
jamsinclair <jamie@vendhq.com>
2016-10-25 11:49:44 +1300

    Serve the working form template and log headers on post

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app = require('express')();
 var multer = require('multer');
+var path = require('path');
 
 var upload = multer({ dest: './uploads' })
 
-app.post('/upload', upload.any(), function (req, res, next) {
+app.get('/', function (req, res, next) {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.post('/upload', upload.array(), function (req, res, next) {
+  console.log(req.headers)
   console.log('body data:', req.body);
   console.log('files data:', req.files);
   res.sendStatus(200);
